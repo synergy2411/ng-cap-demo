1f4093f7adb8eed1efec4ee71ed2873929b4fcc6
synergy2411 <synergy2411@outlook.com>
2018-09-20 18:02:52 +0530

    Initial commit

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : "app-users",
+    template : `<h1>Users Component Loaded Successfully!</h1>`
+})
+export class UsersComponent{}
